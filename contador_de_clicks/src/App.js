af54bbda81961eee1f17318f74e8b4b90295d400
Simon G. Flores <simonflores_10@hotmail.com>
2023-11-22 17:08:19 +0100

    ya tengo creado los botones y funciona porque el console.log responde, debo eliminar eso y seguir

--- contador_de_clicks/src/App.js
+++ contador_de_clicks/src/App.js
@@ -1,10 +1,35 @@
-
 import './App.css';
+import Boton from './componentes/boton';
+import logo from './imagenes/contador.png';
 
 function App() {
+
+  const manejarClick = ()=>{
+    console.log('Click');
+  }
+  const reiniciarContador=()=>{
+    console.log('reiniciar');
+  }
+
   return (
     <div className="App">
-      
+      <div className='logo-contenedor'>
+        <img
+        className='logo'
+        src={logo}
+        alt='Logo principal'/>
+      </div>
+      <div className='contenedor-principal'>
+        <Boton
+        texto='Click'
+        esBotonDeClick={true}
+        manejarClick={manejarClick}/>
+        <Boton
+        texto='Reiniciar'
+        esBotonDeClick={false}
+        manejarClick={reiniciarContador}/>
+
+      </div>
     </div>
   );
 }
